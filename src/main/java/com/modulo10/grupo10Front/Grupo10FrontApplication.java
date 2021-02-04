ff4bce3c41de70589829b5cb4a7a3143adf2338b
alex-vinicio <alexandino2011@gmail.com>
2021-02-04 09:05:35 -0500

    actaulizacion del repositorio para el front-end

---
+++ src/main/java/com/modulo10/grupo10Front/Grupo10FrontApplication.java
@@ -0,0 +1,13 @@
+package com.modulo10.grupo10Front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo10FrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo10FrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modulo10/grupo10Front/Grupo10FrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modulo10.grupo10Front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo10FrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
