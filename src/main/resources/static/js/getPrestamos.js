372b07bfa8b5711d6c7791512280f48ec6c4619a
Diego <Diego@DESKTOP-81LEFDQ>
2021-02-18 22:11:52 -0500

    Htmls

--- src/main/resources/static/js/getPrestamos.js
+++ src/main/resources/static/js/getPrestamos.js
@@ -24,6 +24,7 @@ $(document).ready(function () {
                         }
                     });
                 }); 
+                //comentario
 });
 function generalTable(data){
 	$('#listadoPrestamo').innerHTML="";
